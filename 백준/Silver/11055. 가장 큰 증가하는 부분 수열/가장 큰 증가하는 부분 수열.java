f92bdc15b81771973da4c14b8470ae77a26dad91
ParkChaeSeong <97psh@naver.com>
2024-01-12 18:46:22 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 260 ms, Memory: 18900 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        /*
+        * 시간 제한 : 1초 -> 1억 연산 이하
+        * 수열 크기 : 1000 => 연산 횟수는 1000! => 브루트 포스 불가능
+        * 따라서 다른 방법
+        *
+        * 1. DP
+        * 2...?
+        *
+        * */
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(in.readLine());
+        int[] a = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] dp = a.clone();
+
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i] && dp[i] < dp[j] + a[i]) {
+                    dp[i] = dp[j] + a[i];
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < a.length; i++)
+            answer = Math.max(answer, dp[i]);
+
+        System.out.println(answer);
+    }
+}
