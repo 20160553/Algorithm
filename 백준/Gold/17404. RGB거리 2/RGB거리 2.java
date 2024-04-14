8bbf5ffa2ea9b16adf1c50cb50bf12c987cde39b
ParkChaeSeong <97psh@naver.com>
2024-04-14 09:18:39 +0900

    [Gold IV] Title: RGB거리 2, Time: 140 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         /*
-        * 앞 뒤와 색깔이 같지 않아야 한다.
-        *
-        * 맨 처음이 특정 색깔일 때,
-        * 몇 번 집이,
-        * 몇 번 색일 때 비용을 저장 (3차원 배열)
-        *
-        * */
+         * 앞 뒤와 색깔이 같지 않아야 한다.
+         *
+         * 맨 처음이 특정 색깔일 때,
+         * 몇 번 집이,
+         * 몇 번 색일 때 비용을 저장 (3차원 배열)
+         *
+         * */
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
@@ -21,7 +21,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int answer = MAX_COST;
         int[][] map = new int[n][3];
-        int[][][] dp = new int[3][n][3];
+        int[][] dp = new int[n][3];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -30,36 +30,29 @@ public class Main {
             }
         }
 
-        for (int i = 0; i < 3; i++) {
+
+        for (int k = 0; k < n; k++) {
             for (int j = 0; j < 3; j++) {
-                dp[i][0][j] = MAX_COST;
-                if (i == j) dp[i][0][j] = map[0][i];
-            }
-            for (int k = 1; k < n; k++) {
-                for (int j = 0; j < 3; j++) {
-                    dp[i][k][j] = MAX_COST;
-                }
+                dp[k][j] = MAX_COST;
             }
         }
 
         for (int firstColor = 0; firstColor < 3; firstColor++) {
+            dp[0][firstColor] = map[0][firstColor];
+            dp[0][(firstColor + 1) % 3] = MAX_COST;
+            dp[0][(firstColor + 2) % 3] = MAX_COST;
             for (int i = 1; i < n; i++) {
                 for (int currentColor = 0; currentColor < 3; currentColor++) {
-                    for (int previousColor = 0; previousColor < 3; previousColor++) {
-                        if (currentColor == previousColor) continue;
-                        dp[firstColor][i][currentColor] = Math.min(dp[firstColor][i][currentColor], dp[firstColor][i - 1][previousColor] + map[i][currentColor]);
-                    }
+                    dp[i][currentColor] = MAX_COST;
+                    dp[i][currentColor] = Math.min(dp[i][currentColor], dp[i - 1][(currentColor + 1) % 3] + map[i][currentColor]);
+                    dp[i][currentColor] = Math.min(dp[i][currentColor], dp[i - 1][(currentColor + 2) % 3] + map[i][currentColor]);
                 }
             }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i == j) continue;
-                answer = Math.min(answer, dp[i][n - 1][j]);
+            for (int i = 0; i < 3; i++) {
+                if (firstColor != i)
+                    answer = Math.min(answer, dp[n - 1][i]);
             }
         }
-
         System.out.println(answer);
     }
 }
