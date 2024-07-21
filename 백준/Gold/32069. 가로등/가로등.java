200c6849f0187758690843525241a5f1844146e6
ParkChaeSeong <97psh@naver.com>
2024-07-21 13:58:27 +0900

    [Gold V] Title: 가로등, Time: 1068 ms, Memory: 149832 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/32069. 가로등/가로등.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        /*
+        * 2억 이하 연산
+        *
+        * l : double
+        * n : int
+        * k : int
+        *
+        * k <= l + 1
+        *
+        * */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double l;
+        int n, k;
+        l = Double.parseDouble(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        StringBuilder answer = new StringBuilder();
+
+        double[] p = new double[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            p[i] = Double.parseDouble(st.nextToken());
+        }
+
+        int cnt = 0;
+        int idx = 0;
+        boolean flag = p[n - 1] != l;
+        HashSet<Double> set = new HashSet<>();
+        mainLoop: while (cnt < k) {
+            for (int i = 0; i < n; i++) {
+                boolean flag_1 = true;
+
+                if (i == 0) {
+                    if (p[i] - idx < 0) flag_1 = false;
+                } else {
+                    if (p[i] - idx <= p[i - 1] ) flag_1 = false;
+                }
+                if (flag_1 && !set.contains(p[i] - idx)) {
+                    set.add(p[i] - idx);
+                    answer.append(idx + "\n");
+                    cnt++;
+                    if (cnt == k) break mainLoop;
+                }
+
+                if (i == n - 1) {
+                    if (p[i] + idx > l) continue;
+                } else {
+                    if (p[i] + idx >= p[i + 1] ) continue;
+                }
+                if (!set.contains(p[i] + idx)) {
+                    set.add(p[i] + idx);
+                    answer.append(idx + "\n");
+                    cnt++;
+                    if (cnt == k) break mainLoop;
+                }
+            }
+            idx++;
+        }
+
+        System.out.println(answer);
+    }
+}
