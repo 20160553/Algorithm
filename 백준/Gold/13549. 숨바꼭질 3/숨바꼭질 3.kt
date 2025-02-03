fa1434f3d3ff902b088c42a214f2ba3caa37bbf1
ParkChaeSeong <97psh@naver.com>
2025-02-03 13:09:40 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 272 ms, Memory: 45508 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.kt
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.kt
@@ -3,8 +3,9 @@ import java.util.*
 fun main() {
     val (n, k) = readln().split(" ").map { it.toInt() }
 
+    val MAX_POSITION = 100_000
     val q: Queue<IntArray> = LinkedList()
-    val v: MutableMap<Int, Int> = mutableMapOf()
+    val v: IntArray = IntArray(MAX_POSITION + 1) { -1 }
 
     val functions: List<(Int) -> Int> = listOf(
             { x -> 2 * x },
@@ -24,7 +25,7 @@ fun main() {
             val next: IntArray = current.copyOf()
             next[0] = functions[j](current[0])
 
-            if (next[0] > 200_000 || next[0] < 0) continue
+            if (next[0] > MAX_POSITION || next[0] < 0) continue
             if (j != 0) next[1]++
 
             if (next[0] == k) {
@@ -32,7 +33,7 @@ fun main() {
                 return
             }
 
-            if (v[next[0]] == null || v[next[0]]!! > next[1]) {
+            if (v[next[0]] == -1 || v[next[0]] > next[1]) {
                 v[next[0]] = next[1]
                 q.offer(next)
             }
