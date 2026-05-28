bca095bf88e7c9ed185f872bb9a23a7f54125be1
cicada_park <97psh@naver.com>
2026-05-28 22:17:51 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 0.78 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -1,128 +1,57 @@
 import java.util.*;
 
-class Word {
-    String content;
-    int start;
-    int end;
-    boolean isChecked;
-    
-    public Word(String content, int start, int end) {
-        this.content = content;
-        this.start = start;
-        this.end = end;
-        this.isChecked = false;
-    }
-    
-    public boolean isPrev(int start) {
-        return this.end < start;
-    }
-    
-    public boolean isAfter(int end) {
-        return this.start > end;
-    }
-    
-    public boolean isSpolierWord(int s_s, int s_e) {
-        if (s_s <= end || (start <= s_s && s_e <= end)) return true;
-        return false;
-    }
-    
-    public boolean equals(Word word) {
-        if (this.content.equals(word.content)) return true;
-        return false;
-    }
-    
-    
-    public String toString() {
-        return content + " " + start + "-" + end;
-    }
-}
-
 class Solution {
     public int solution(String message, int[][] spoiler_ranges) {
-        /*
-        1. 일부 문자만 스포 방지더라도 전체가 스포방지
+        int answer = 0;
+        int n = message.length();
         
-        1. end < spoiler_start 인 경우는 단어 idx++, 
-        2. 스포일러 단어에 걸치는 경우, 스포일러 인덱스, 단어 인덱스 둘다 ++, 중요 체크
-        3. 반복
+        HashSet<String> unSignificants = new HashSet<>();
+        HashSet<String> significants = new HashSet<>();
         
-        */
+        boolean[] spoilerFlag = new boolean[n];
         
-        // 초기화
-        List<Word> words = new ArrayList<>();
-        HashSet<String> importantWords = new HashSet<>();
-        HashSet<String> unimportantWords = new HashSet<>();
+        for (int i = 0; i < spoiler_ranges.length; i++) {
+            int start = spoiler_ranges[i][0];
+            int end = spoiler_ranges[i][1];
+            
+            for (int j = start; j <= end; j++) {
+                spoilerFlag[j] = true;
+            }
+        }
         
-        int s = -1;
-        String str = "";
-        for (int i = 0; i < message.length(); i++) {
+        int start = -1;
+        boolean flag = false;
+        for (int i = 0; i < n; i++) {
             char c = message.charAt(i);
             if (c == ' ') {
-                if (s == -1) continue;
-                words.add(new Word(str, s, i - 1));
-                s = -1;
-                str = "";
+                if (start == -1) continue;
+                String str = message.substring(start, i);
+                // System.out.println(str);
+                if (flag) significants.add(str);
+                else unSignificants.add(str);
+                start = -1;
+                flag = false;
                 continue;
             }
-            if (s == -1) s = i;
-            str += c;
+            if (start == -1) start = i;
+            flag |= spoilerFlag[i];
         }
-        if (s != -1) words.add(new Word(str, s, message.length() - 1));
         
-        System.out.println("words: " + words);
+        if (start != -1) {
+            String str = message.substring(start, n);
+            // System.out.println(str);
+            if (flag) significants.add(str);
+            else unSignificants.add(str);
+            start = -1;
+            flag = false;
+        }
         
-        // 중요, 안중요 구분
-        int wordsIdx = 0;
-        for (int spoilerIdx = 0; spoilerIdx < spoiler_ranges.length; spoilerIdx++) {
-            int[] spoiler = spoiler_ranges[spoilerIdx];
-            int s_s = spoiler[0];
-            int s_e = spoiler[1];
-            while (wordsIdx < words.size()) {
-                Word w = words.get(wordsIdx);
-                System.out.println("wordsContent~!" + w.content);
-                if (w.isPrev(s_s)) {
-                    wordsIdx++;
-                    importantWords.remove(w.content);
-                    unimportantWords.add(w.content);
-                    w.isChecked = true;
-                    continue;
-                }
-                if (w.isAfter(s_e)) {
-                    if (spoilerIdx == spoiler_ranges.length) {
-                        importantWords.remove(w.content);
-                        unimportantWords.add(w.content);
-                    }
-                    break;
-                }
-                boolean isSpoiler = w.isSpolierWord(s_s, s_e);
-                wordsIdx++;
-                w.isChecked = true;
-                if (isSpoiler) {
-                    if (!unimportantWords.contains(w.content)) {
-                        importantWords.add(w.content);
-                    } else {
-                        importantWords.remove(w.content);
-                        unimportantWords.add(w.content);
-                    }
-                } else {
-                    importantWords.remove(w.content);
-                    unimportantWords.add(w.content);
-                }
+        for (String str: significants) {
+            if (!unSignificants.contains(str)) {
+                answer++;
             }
         }
-        System.out.println(wordsIdx);
-        while (wordsIdx < words.size()) {
-            Word w = words.get(wordsIdx);
-            System.out.println("wordsContent2~!" + w.content);
-            wordsIdx++;
-            w.isChecked = true;
-
-            importantWords.remove(w.content);
-            unimportantWords.add(w.content);
-        }
-        System.out.println("unimport: " + unimportantWords);
-        System.out.println("import: " + importantWords);
         
-        return importantWords.size();
+        return answer;
     }
 }
